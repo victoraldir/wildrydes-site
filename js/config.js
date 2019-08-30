8c0b360e6748f34fd0a5b30af6488b3ed8399bd1
Victor Hugo <vhaldir@gmail.com>
2019-08-30 11:08:28 -0300

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_6CrOl1MqZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4vkifmpps0aivv9p20k98o55nt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
